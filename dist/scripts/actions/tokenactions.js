c3157be42fd03f651af177ed73619b527a2e0461
spacemandev <devbharel@gmail.com>
2021-01-04 14:04:54 -0600

    token drag drop actions work

--- dist/scripts/actions/tokenactions.js
+++ dist/scripts/actions/tokenactions.js
@@ -11,13 +11,59 @@ Hooks.on("renderTokenHUD", (tokenHUD, html, opts) => __awaiter(this, void 0, voi
     console.log(tokenHUD, html, opts);
     //@ts-ignore
     let currentActor = game.actors.get(opts.actorId);
-    const actionsButton = $(`<i class="control-icon fa fas-gavel swade-hud-actions" title=${game.i18n.localize("SWADE_Actions.Actions")}></i>`);
+    const actionsButton = $(`<i id='actionsList' class="control-icon fas fa-gavel swade-hud-actions" title=${game.i18n.localize("SWADE_Actions.Actions")}></i>`);
     //@ts-ignore
-    let actionsList = yield renderTemplate('modules/swade-toolkit/templates/ActionsList.hbs', getActionsList(Array.from(currentActor.items)));
+    let actionsList = yield renderTemplate('modules/swade-toolkit/templates/ActionsList.hbs', getActionsList(currentActor, Array.from(currentActor.items)));
     actionsButton.append(actionsList);
     html.find(".right").append(actionsButton);
+    //make the thingies drag droppable
+    //@ts-ignore
+    const dragDrop = new DragDrop({
+        dragSelector: ".swade-action",
+        dropSelector: "#board",
+        callbacks: {
+            dragstart: (evt) => __awaiter(this, void 0, void 0, function* () {
+                evt.dataTransfer.setData("text/plain", JSON.stringify(evt.target.dataset));
+            }),
+            drop: handleActionDrop
+        }
+    });
+    //@ts-ignore
+    dragDrop.bind(document);
 }));
-function getActionsList(actorItems) {
+function handleActionDrop(evt) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let data = JSON.parse(evt.dataTransfer.getData('text/plain'));
+        console.log("Data: ", data); //itemId, actionId, actorId
+        const getTokenAtXY = (clientX, clientY) => {
+            let token = undefined;
+            let gridSize = game.scenes.active.data['grid'];
+            const t = canvas.stage.worldTransform;
+            let x = (clientX - t.tx) / canvas.stage.scale.x;
+            let y = (clientY - t.ty) / canvas.stage.scale.y;
+            const inRange = (val, min, max) => {
+                let inRange = (val >= min && val <= max) ? true : false;
+                return inRange;
+            };
+            for (let tok of canvas.tokens.placeables) {
+                if (inRange(x, tok.position._x, tok.position._x + gridSize) && inRange(y, tok.position._y, tok.position._y + gridSize)) {
+                    return tok;
+                }
+            }
+            return token;
+        };
+        let token = getTokenAtXY(evt.clientX, evt.clientY);
+        console.log("Token:", token);
+        //Set the dropped token as target
+        //Doesn't do anything, assumes transformers will need this information for their magic
+        token.setTarget(true, game.user, true, false);
+        //handle the action
+        let actor = game.actors.get(data.actorId);
+        let item = actor.items.find(el => el.id == data.itemId);
+        game.swade.SwadeItem._handleAdditionalActions(item, actor, data.actionId);
+    });
+}
+function getActionsList(actor, actorItems) {
     let actionsList = [];
     for (let item of actorItems) {
         if (item.data.data.actions && item.data.data.equipped) {
@@ -28,6 +74,7 @@ function getActionsList(actorItems) {
                     name: item.name + ":" + action.name,
                     img: item.img,
                     itemID: item.id,
+                    actorID: actor.id,
                     actionID: key
                 }); // don't need anything else as I can look it up with Item+ActionID (already know Actor for selected Token)
             }
--- src/scripts/actions/tokenactions.ts
+++ src/scripts/actions/tokenactions.ts
@@ -3,9 +3,9 @@ Hooks.on("renderTokenHUD", async (tokenHUD:TokenHUD, html:any, opts:any) => {
 
   //@ts-ignore
   let currentActor = game.actors.get(opts.actorId);
-  const actionsButton = $(`<i class="control-icon fas fa-gavel swade-hud-actions" title=${game.i18n.localize("SWADE_Actions.Actions")}></i>`)
+  const actionsButton = $(`<i id='actionsList' class="control-icon fas fa-gavel swade-hud-actions" title=${game.i18n.localize("SWADE_Actions.Actions")}></i>`)
   //@ts-ignore
-  let actionsList = await renderTemplate('modules/swade-toolkit/templates/ActionsList.hbs', getActionsList(Array.from(currentActor.items)))
+  let actionsList = await renderTemplate('modules/swade-toolkit/templates/ActionsList.hbs', getActionsList(currentActor, Array.from(currentActor.items)))
   actionsButton.append(actionsList)
   html.find(".right").append(actionsButton);
 
@@ -14,14 +14,53 @@ Hooks.on("renderTokenHUD", async (tokenHUD:TokenHUD, html:any, opts:any) => {
   const dragDrop = new DragDrop({
     dragSelector: ".swade-action",
     dropSelector: "#board",
-    permissions: { dragstart: this._canDragStart.bind(this), drop: this._canDragDrop.bind(this) },
-    callbacks: { dragstart: this._onDragStart.bind(this), drop: this._onDragDrop.bind(this) }
+    callbacks: {
+      dragstart: async (evt) => {
+        evt.dataTransfer.setData("text/plain", JSON.stringify(evt.target.dataset))
+      },
+      drop: handleActionDrop
+    }
   });
   //@ts-ignore
-  dragDrop.bind(html);
+  dragDrop.bind(document);
 })  
 
-function getActionsList(actorItems:Item[]){
+async function handleActionDrop(evt:DragEvent){
+  let data = JSON.parse(evt.dataTransfer.getData('text/plain'));
+  console.log("Data: ", data) //itemId, actionId, actorId
+  const getTokenAtXY = (clientX, clientY) => {
+    let token:Token = undefined;
+    let gridSize = game.scenes.active.data['grid'];
+    const t = canvas.stage.worldTransform;
+    let x = (clientX - t.tx) / canvas.stage.scale.x;
+    let y = (clientY - t.ty) / canvas.stage.scale.y;
+
+    const inRange = (val, min, max) => {
+      let inRange = (val>=min && val<=max) ? true : false;
+      return inRange;
+    }
+
+    for(let tok of canvas.tokens.placeables){
+      if(inRange(x,tok.position._x, tok.position._x+gridSize) && inRange(y,tok.position._y,tok.position._y+gridSize)){
+        return tok;
+      }
+    }
+    return token;
+  }
+  let token = getTokenAtXY(evt.clientX, evt.clientY);
+  console.log("Token:", token);
+  
+  //Set the dropped token as target
+  //Doesn't do anything, assumes transformers will need this information for their magic
+  token.setTarget(true, game.user, true, false);
+
+  //handle the action
+  let actor = game.actors.get(data.actorId);
+  let item = actor.items.find(el => el.id == data.itemId);
+  game.swade.SwadeItem._handleAdditionalActions(item, actor, data.actionId);
+}
+
+function getActionsList(actor:Actor, actorItems:Item[]){
   let actionsList = []
   for(let item of actorItems){
     if(item.data.data.actions && item.data.data.equipped){
@@ -32,6 +71,7 @@ function getActionsList(actorItems:Item[]){
           name: item.name+":"+action.name,
           img: item.img,
           itemID: item.id,
+          actorID: actor.id,
           actionID: key
         }) // don't need anything else as I can look it up with Item+ActionID (already know Actor for selected Token)
       }
